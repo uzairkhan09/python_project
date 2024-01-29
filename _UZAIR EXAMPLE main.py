ad41398ddbc93506a8812a74b331806385288c3b
uzairkhan09 <157619802+uzairkhan09@users.noreply.github.com>
2024-01-29 21:03:35 +0500

    Add files via upload

---
+++ _UZAIR EXAMPLE main.py
@@ -0,0 +1,9 @@
+a = int(input("Enter number a: "));
+b = int(input("Enter number b: "));
+c = int(input("Enter number c: "));
+if a>b and a>c:
+    print(" Fronm the above three numbers given a is largest");
+if b>a and b>c:
+     print(" Fronm the above three numbers given b is largest");
+if c>a and c>b:
+    print(" Fronm the above three numbers given c is largest");
---
+++ uzair khan example 2.py
@@ -0,0 +1,6 @@
+age = int (input("ENTER age:"));
+if age >= 18 :
+    print("you are able to  vote");
+else:
+    print("you are not able for vote");
+    
