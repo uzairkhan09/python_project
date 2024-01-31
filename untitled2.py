cfb09b382a888c6bad5338c0f4e14c0ba8d10a51
uzairkhan09 <157619802+uzairkhan09@users.noreply.github.com>
2024-01-31 21:24:21 +0500

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,11 @@
+class python:
+    def __init__(self,name,company):
+        self.name=name
+        self.company=company
+    def show(self):
+        print("Hello my name is "+self.name + " and I " +" work in "+self.company+".")
+obj=python("Hassan", "Bano Qabil")
+obj.show()
+
+
+      
