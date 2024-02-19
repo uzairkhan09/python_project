a6d3670663feec3dc8861e5fd69b73c33c3e06d4
uzairkhan09 <157619802+uzairkhan09@users.noreply.github.com>
2024-02-19 21:19:32 +0500

    Add files via upload

---
+++ untitled17.py
@@ -0,0 +1,9 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Am I HTML already?</title>
+</head>
+<body>
+Yes,<br>I am!uzair khan father of arham ali and son of md. sharf alam khan app sab ko salam
+</body>
+</html>
