791edb543e8409dacc3afecc1b233c357ced6298
uzairkhan09 <157619802+uzairkhan09@users.noreply.github.com>
2024-02-28 21:12:56 +0500

    Delete uzair example 3.py

--- uzair example 3.py
+++
@@ -1,9 +0,0 @@
-number = int(input("enter the number?"))
-if number <=10:
-    print("the given numberis equals to 10")
-elif number ==50:
-    print("the given numberis equals to 50")
-elif number ==100:
-    print("the given numberis equals to 100")
-else:
-    print("the given number is not equals to10,50 and 100")
