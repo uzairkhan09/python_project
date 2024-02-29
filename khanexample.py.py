85e8d344a869ec5cc96a87ee4e94cd45d1849765
uzairkhan09 <157619802+uzairkhan09@users.noreply.github.com>
2024-02-29 13:40:43 +0500

    Update khanexample.py.py

--- khanexample.py.py
+++ khanexample.py.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 # Function to display available menu items with images
 def display_menu():
+    st.title("banoqabil 2.0 final project")
     st.header("Menu")
     st.subheader("Select items to order:")
     
