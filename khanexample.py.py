2994e6bf1888dede87b30552bd17703f7da8ef43
uzairkhan09 <157619802+uzairkhan09@users.noreply.github.com>
2024-02-29 13:50:18 +0500

    Update khanexample.py.py

--- khanexample.py.py
+++ khanexample.py.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Function to display available menu items with images
-def menu_display():
+def display_menu():
     st.title("banoqabil 2.0 final project")
     st.header("Menu")
     st.subheader("Select items to order:")
